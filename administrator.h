9d4b661fc69ed8d01328ad441d9d9a3ea15d1f08
williamDenson <willde4@mail.regent.edu>
2024-05-22 21:51:32 -0400

    Specification file administrator.h
    Just experimenting with the class structure. Feel free to change up

---
+++ administrator.h
@@ -0,0 +1,31 @@
+// Specification file for administrator
+#ifndef ADMINISTRATOR_H_INCLUDED
+#define ADMINISTRATOR_H_INCLUDED
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+class administrator{
+
+
+
+    public:
+
+        // Standard setter and getter functions.
+        // ...
+
+
+
+    private:    // or protected?
+
+
+};
+
+
+
+
+
+#endif
