cb62f9bc22acdb5232f8b21f98de83c507ec6706
williamDenson <willde4@mail.regent.edu>
2024-06-04 21:48:27 -0400

    Update BinarySearchTree.h - added some ADT operations

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -28,6 +28,38 @@ public:
     void addBook(Book* book);
     void displayBooks() const;
     Book* searchBook(std::string isbn) const;
+
+
+//----------------------------------------------------------------
+// Public methods con't. for review.
+
+    bool isEmpty() const;
+    int treeHeight() const;
+    //...
+
+
+    //-------------------------------------------------------------------------------------------------------
+    // Leaf/node count from root could be used to help aid in efficient Big-O algorithm sorting evaluations?
+    int treeNodeCount() const;
+    int treeLeavesCount() const;
+    //-------------------------------------------------------------------------------------------------------
+    //...
+
+
+    //----------------------------------------------------
+    // Helping prevent memory leaks and dangling pointers.
+    void destroyTree();            // Will delete nodes & set to NULL/nullptr before rearrangement/shuffling and deallocate memory for future dereferencing.
+
+    virtual void insert(const Book* book insertItem) = 0;        // I am not sure if the formal parameter class type/template/data type is correct.
+
+    virtual void deleteNode(const Book* book deleteItem) = 0;
+
+    ~BookBST();
+//----------------------------------------------------------------
+
+
+
+
 };
 
 #endif //BINARY_SEARCH_TREE_H
